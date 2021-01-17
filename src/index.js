5ac9abbc95b30ca2531d51b8d99e6e08c2867ae4
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-17 17:43:04 +0300

    to vercle fix

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import vkBridge from '@vkontakte/vk-bridge';
 import * as backend from "./api";
 import * as router from "./router";
 import App from './app/components/App/AppContainer';
-//
-// if (process.env.NODE_ENV === 'development') {
-//     const whyDidYouRender = require('@welldone-software/why-did-you-render');
-//     whyDidYouRender(React, {
-//         trackAllPureComponents: true,
-//         trackExtraHooks: [[require("react-redux/lib"), "useSelector"]]
-//     });
-// }
+
+if (process.env.NODE_ENV === 'development') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render');
+    whyDidYouRender(React, {
+        trackAllPureComponents: true,
+        trackExtraHooks: [[require("react-redux/lib"), "useSelector"]]
+    });
+}
 
 // Init VK Mini App
 vkBridge.send('VKWebAppInit');
