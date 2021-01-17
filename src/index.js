79fd5a07992516a1b19987bd1d45ed0a2af50226
Вадик Мельниченко <melnicenkovadik@gmail.com>
2021-01-17 17:03:57 +0300

    to vercle fix

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import 'core-js/features/map';
 import 'core-js/features/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { getStore } from "./app/store";
+import {getStore} from "./app/store";
 import vkBridge from '@vkontakte/vk-bridge';
 import * as backend from "./api";
 import * as router from "./router";
 import App from './app/components/App/AppContainer';
-
-if (process.env.NODE_ENV === 'development') {
-    const whyDidYouRender = require('@welldone-software/why-did-you-render');
-    whyDidYouRender(React, {
-        trackAllPureComponents: true,
-        trackExtraHooks: [[require("react-redux/lib"), "useSelector"]]
-    });
-}
+//
+// if (process.env.NODE_ENV === 'development') {
+//     const whyDidYouRender = require('@welldone-software/why-did-you-render');
+//     whyDidYouRender(React, {
+//         trackAllPureComponents: true,
+//         trackExtraHooks: [[require("react-redux/lib"), "useSelector"]]
+//     });
+// }
 
 // Init VK Mini App
 vkBridge.send('VKWebAppInit');
@@ -23,4 +23,4 @@ const route = router.initialize();
 const store = getStore();
 backend.initialize();
 
-ReactDOM.render(<App router={route} store={store} />, document.getElementById('root'));
+ReactDOM.render(<App router={route} store={store}/>, document.getElementById('root'));
